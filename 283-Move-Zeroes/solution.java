403852f6379328d3fcb460334309153fea079848
Brian-0114 <boyuran2012@u.northwestern.edu>
2016-07-12 16:56:34 -0700

    [283. Move Zeroes][Accepted]committed by LeetCode Extension

---
+++ 283-Move-Zeroes/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        while(j<nums.length){
+            if(nums[j]==0) j++;
+            else{
+                nums[i] = nums[j];
+                i++;
+                j++;
+            }
+            
+        }
+        
+        while(i<nums.length){
+            nums[i]=0;
+            i++;
+        }
+    }
+}
