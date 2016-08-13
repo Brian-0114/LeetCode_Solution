21db1627d4eb88bf50c25433812b8442a8b98979
Brian-0114 <boyuran2012@u.northwestern.edu>
2016-08-12 19:57:40 -0700

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Accepted]committed by LeetCode Extension

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -8,35 +8,40 @@
  * }
  */
 public class Solution {
-  public TreeNode buildTree(int[] preorder, int[] inorder) {
-    int preStart = 0;
-    int preEnd = preorder.length-1;
-    int inStart = 0;
-    int inEnd = inorder.length-1;
- 
-    return construct(preorder, preStart, preEnd, inorder, inStart, inEnd);
-}
- 
-public TreeNode construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
-    if(preStart>preEnd||inStart>inEnd){
-        return null;
-    }
- 
-    int val = preorder[preStart];
-    TreeNode p = new TreeNode(val);
- 
-    //find parent element index from inorder
-    int k=0;
-    for(int i=0; i<inorder.length; i++){
-        if(val == inorder[i]){
-            k=i;
-            break;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (inorder.length != preorder.length) {
+            return null;
         }
+        
+        int preStart  = 0;
+        int preEnd = preorder.length -1;
+        int inStart = 0;
+        int inEnd = inorder.length -1;
+        
+        return construct(preorder,preStart,preEnd,inorder,inStart,inEnd);
     }
- 
-    p.left = construct(preorder, preStart+1, preStart+(k-inStart), inorder, inStart, k-1);
-    p.right= construct(preorder, preStart+(k-inStart)+1, preEnd, inorder, k+1 , inEnd);
- 
-    return p;
-}
+    
+    public TreeNode construct(int[] preorder,int preStart,int preEnd, int[] inorder,int inStart, int inEnd){
+        if(inStart > inEnd) return null;
+        
+        int val = preorder[preStart];
+        TreeNode p = new TreeNode(val);
+        
+        
+        int k = 0;
+        for(int i=0;i<=inorder.length;i++){
+            if(val==inorder[i]){
+               k = i;
+               break;
+                
+            }
+        }
+        
+        p.left = construct(preorder,preStart+1,preStart+k-inStart,inorder,inStart,k-1);
+        p.right = construct(preorder,preStart+(k-inStart)+1,preEnd,inorder,k+1,inEnd);
+        
+        return p;
+        
+        
+    }
 }
