3a12e60e2e45d40dc22b3d374b3780ee8471ec3d
Brian-0114 <boyuran2012@u.northwestern.edu>
2016-08-15 22:24:57 -0700

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int climbStairs(int n) {
         if(n==0 || n==1 || n==2) return n;
         int[] p = new int[n+1];
-        p[0] = 0;
+        p[0] = 1;
         p[1] = 1;
         p[2] = 2;
         int res = Math.max(p[0],Math.max(p[1],p[2]));
